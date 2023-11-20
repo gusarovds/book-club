55a9feb758c4023b090cc098758d8763d65ca767
Dmitrii Gusarov <gusarovds@ya.ru>
2023-11-21 01:09:34 +0300

    add tooltip at timeline

--- book-club/src/components/Timeline/Timeline.tsx
+++ book-club/src/components/Timeline/Timeline.tsx
@@ -1,13 +1,16 @@
 import { selectCurrentPosts } from "@/store/posts";
 import { useSelector } from "react-redux";
 import Link from 'next/link'
+import { Tooltip as TooltipUi } from '@mui/material'
 
 const Timeline = () => {
     const posts = useSelector(selectCurrentPosts);
 
     return <div className="flex flex-row-reverse justify-between items-center m-4 h-1 w-10/12 bg-gray-50 rounded mx-auto my-10">
         {posts.map((post, i) => <Link key={i} href={`#${post.anchor}`} className="h-8 w-8 rounded-full bg-gray-50 flex items-center justify-center cursor-pointer text-xl">
-            {post.emoji}
+            <TooltipUi placement="bottom" title={`${post.author} - ${post.title}`}>
+                <span>{post.emoji}</span>
+            </TooltipUi>
         </Link>)}
     </div>
 }
